40491bafb24946d5ecbce980457f8da47a70bbaf
mircosmaker <99589292+mircosmaker@users.noreply.github.com>
2022-03-26 11:25:05 +0100

    codice di micillo

---
+++ Socket1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package socket1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author studente
+ */
+public class Socket1 {
+    
+    
+    public static void main(String[] args) throws IOException {
+        Socket s=new Socket("192.168.20.29",40000);//
+        
+    }
+    
+    
+}
+
